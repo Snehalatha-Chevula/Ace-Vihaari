d2e734aaa829546ca21c224cb441a8d40c40a100
Rajesh-Gundu <mail2rajeshgundu@gmail.com>
2025-06-09 12:58:01 +0530

    updated the axios.defaults.baseURL in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { UserProvider } from "./context/userContext";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://ace-vihaari.onrender.com"; // Adjust if needed
+axios.defaults.baseURL = import.meta.env.VITE_API_URL; // Adjust if needed
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
